d9272a99fcd5d030e4ec22683ac3c1c838c6c82a
SURYA IGOR <suryasundarraj@users.noreply.github.com>
2015-10-01 01:25:02 +0530

    Update main.c

--- Digital/ledBlink/main.c
+++ Digital/ledBlink/main.c
@@ -1,21 +1,27 @@
+/*********************************************************************************
+Atmega8 LED Blink Program
+*********************************************************************************/
 //Specify the Functional Clock with respectie External Crystal 
 #define F_CPU 16000000UL		            //16 MHz Clock
-
 //Import the Libiraries Required
 #include <avr/io.h>
 #include <util/delay.h>
-
-//Main Program
+/*********************************************************************************
+Function Name     : main
+Description       : Initialize the pin as a OUTPUT and Blink the LED repeatedly 
+Parameters        : void
+Return            : void
+*********************************************************************************/
 int main(void)
 {
-  DDRB |= (1<<PINB0);                   //Setting the Data Direction for the Specfic Pin Used as Output
-  PORTB |= (1<<PINB0);                  //Initial High 
-
+  DDRB |= (1<<PINB0);         //Setting the Data Direction for the Specfic Pin Used as Output
+  PORTB |= (1<<PINB0);        //Initial High 
   while (1) {
-    PORTB ^= (1<<PINB0);                //Toggling the Pin using the EX-OR Bit Wise Operator
-    _delay_ms(500);                     //500 ms Delay
+    PORTB ^= (1<<PINB0);      //Toggling the Pin using the EX-OR Bit Wise Operator
+    _delay_ms(500);           //500 milli seconds delay
   }
 }
+
 //End of the Program
 
 
